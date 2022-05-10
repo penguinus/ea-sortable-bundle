e580d53211615f08572f61195cd320cbfff3c23e
Azat <sheredeka_azat@mail.ru>
2022-05-10 20:58:27 +0400

    Update SortableController.php

--- Controller/SortableController.php
+++ Controller/SortableController.php
@@ -3,7 +3,10 @@
 namespace Orkestra\EaSortable\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Option\EA;
 use EasyCorp\Bundle\EasyAdminBundle\Configuration\ConfigManager;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,20 +17,17 @@ class SortableController extends AbstractController
 {
     /**
      * @Route("/admin/sort/{property}", name="easyadmin.sortable.sort")
-     * @param Request $request
-     * @param ConfigManager $configManager
-     * @param EntityManagerInterface $em
-     * @param string $property
-     * @return RedirectResponse
      */
-    public function sort(Request $request, ConfigManager $configManager, EntityManagerInterface $em, string $property): RedirectResponse
+    public function sort(Request $request, EntityManagerInterface $em, string $property): RedirectResponse
     {
-        $entity = $request->get('entity');
         $id = $request->get('id');
         $position = (int)$request->get('position');
-        $config = $configManager->getEntityConfig($entity);
-        $object = $em->find($config['class'], $id);
-        if (null === $object) throw $this->createNotFoundException();
+        $adminContext = $request->attributes->get(EA::CONTEXT_REQUEST_ATTRIBUTE);
+        $object = $em->find($request->query->get('entity'), $id);
+
+        if (null === $object) {
+            throw $this->createNotFoundException();
+        }
 
         $accessor = PropertyAccess::createPropertyAccessor();
         $accessor->setValue($object, $property, $position);
