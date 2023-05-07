f83f5bdf50e05e1671117bff463d7e73ef3251db
Michael N <penguinus@gmail.com>
2023-05-07 22:36:47 +0300

    Fix return type for PHP 8 and Symfony 6

--- OrkestraEaSortableBundle.php
+++ OrkestraEaSortableBundle.php
@@ -3,11 +3,12 @@
 namespace Orkestra\EaSortable;
 
 use Orkestra\EaSortable\DependencyInjection\OrkestraEaSortableExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class OrkestraEaSortableBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new OrkestraEaSortableExtension();
     }
